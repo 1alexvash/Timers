8fe6a71687345d350b894b2cbe84ac05213a1b5c
Alexander <1alexvash@gmail.com>
2020-03-16 19:04:10 +0200

    Updates

---
+++ precache-manifest.85d51cbf5645ced25c28c7edd075c4eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc62593e210f6454f1be13bb3c903bce",
+    "url": "/Timers/index.html"
+  },
+  {
+    "revision": "c1325a4d87da7a4d0eff",
+    "url": "/Timers/static/css/main.a51f887f.chunk.css"
+  },
+  {
+    "revision": "895f8580996a0ff0f313",
+    "url": "/Timers/static/js/2.21094d88.chunk.js"
+  },
+  {
+    "revision": "8adee38dab91e3f8f57d3fadb72f3d01",
+    "url": "/Timers/static/js/2.21094d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1325a4d87da7a4d0eff",
+    "url": "/Timers/static/js/main.e99cbc6c.chunk.js"
+  },
+  {
+    "revision": "e998cbb38daee5977fad",
+    "url": "/Timers/static/js/runtime-main.00670585.js"
+  },
+  {
+    "revision": "8a415529c71f331ec8eed73f97da6bda",
+    "url": "/Timers/static/media/soundOff.8a415529.png"
+  },
+  {
+    "revision": "b2fc898ff8fe0c95ddf3eabdce9bdf6f",
+    "url": "/Timers/static/media/soundOn.b2fc898f.png"
+  },
+  {
+    "revision": "50d6852de841c069808f40e9e3b16fd6",
+    "url": "/Timers/static/media/stopwatch.50d6852d.png"
+  },
+  {
+    "revision": "de1489aad1cda0f8b36560448cb7987b",
+    "url": "/Timers/static/media/timerSound.de1489aa.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6788c7ac0803d1ae56cdf0c86b54710.js"
+  "/Timers/precache-manifest.85d51cbf5645ced25c28c7edd075c4eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Timers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
